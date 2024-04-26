998a57d922b9597900d026b337bc5d1a2347c20f
Okuchukwu Nzeogwu <cpu.governor@gmail.com>
2024-04-26 03:28:52 +0000

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,32 @@
+# import required sub-systems
+from tkinter import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+from tkinter.font import Font
+from tkinter.scrolledtext import *
+
+#import local file
+import file_menu
+import edit_menu
+import format_menu
+import help_menu
+
+root = Tk()
+
+root.title("Light")
+root.geometry("640x480")
+root.minsize(width=400, height=400)
+
+text = ScrolledText(root, state='normal', height=400, width=400, wrap='word', pady=2, padx=3, undo=True)
+text.pack(fill=Y, expand=1)
+text.focus_set()
+
+menubar = Menu(root)
+
+file_menu.main(root, text, menubar)
+edit_menu.main(root, text, menubar)
+format_menu.main(root, text, menubar)
+help_menu.main(root, text, menubar)
+
+#main program loop
+root.mainloop()
