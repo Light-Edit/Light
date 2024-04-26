37fd9d0a9b9a60ff0a2c1718856a6fb087d45d4b
cpu.governor <cpu.governor@gmail.com>
2024-04-26 21:41:48 +0000

    updated main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from tkinter.messagebox import *
 from tkinter.font import Font
 from tkinter.scrolledtext import *
 
-#import local file
+# import local file
 import file_menu
 import edit_menu
 import format_menu
@@ -14,8 +14,8 @@ import help_menu
 root = Tk()
 
 root.title("Light")
-root.geometry("640x480")
-root.minsize(width=400, height=400)
+root.geometry("640x480") #Default resolution
+root.minsize(width=400, height=400) # user can resize the window but won't be able to shrink it smaller than 400px
 
 text = ScrolledText(root, state='normal', height=400, width=400, wrap='word', pady=2, padx=3, undo=True)
 text.pack(fill=Y, expand=1)
